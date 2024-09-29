c2bc3c5b98dff67bfd49c515daad1080b12ab755
Subham Pradhan <59175226+SGsubuuu-2322@users.noreply.github.com>
2024-09-29 17:08:26 +0530

    Setup the codebase or folder structure for the client-side-app...

---
+++ client_app/client-side-app/src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <span className="text-3xl font-bold underline bg-rose-500">
+        Hello world!
+      </span>
+    </>
+  );
+}
+
+export default App;
