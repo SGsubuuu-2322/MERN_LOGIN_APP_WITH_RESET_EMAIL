f15a1ac89e30d16b54436390f6abfd150f413468
Subham Pradhan <59175226+SGsubuuu-2322@users.noreply.github.com>
2024-09-30 23:30:17 +0530

    Added different routes and files and folder in client app...

--- client_app/client-side-app/src/App.jsx
+++ client_app/client-side-app/src/App.jsx
@@ -1,12 +1,55 @@
-import "./App.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+/** import all components */
+import UserName from "./components/UserName";
+import Password from "./components/Password";
+import Register from "./components/Register";
+import Profile from "./components/Profile";
+import Recovery from "./components/Recovery";
+import Reset from "./components/Reset";
+import PageNotFound from "./components/PageNotFound";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <UserName />,
+  },
+  {
+    path: "/register",
+    element: <Register />,
+  },
+  {
+    path: "/password",
+    element: <Password />,
+  },
+  ,
+  {
+    path: "/profile",
+    element: (
+      <AuthorizeUser>
+        <Profile />
+      </AuthorizeUser>
+    ),
+  },
+  {
+    path: "/recovery",
+    element: <Recovery></Recovery>,
+  },
+  {
+    path: "/reset",
+    element: <Reset></Reset>,
+  },
+  {
+    path: "*",
+    element: <PageNotFound></PageNotFound>,
+  },
+]);
 
 function App() {
   return (
-    <>
-      <span className="text-3xl font-bold underline bg-rose-500">
-        Hello world!
-      </span>
-    </>
+    <main>
+      <RouterProvider router={router}></RouterProvider>
+    </main>
   );
 }
 
