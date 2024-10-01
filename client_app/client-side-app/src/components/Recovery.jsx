3dbb9ef57115219aa4e2ea5d57387e22bbd0f46a
Subham Pradhan <59175226+SGsubuuu-2322@users.noreply.github.com>
2024-10-01 20:06:06 +0530

    Added recovery password component...

--- client_app/client-side-app/src/components/Recovery.jsx
+++ client_app/client-side-app/src/components/Recovery.jsx
@@ -1,9 +1,67 @@
-// import React from 'react'
+// import React from "react";
+import { Link } from "react-router-dom";
+import styles from "../styles/Username.module.css";
+import { Toaster } from "react-hot-toast";
+import { useFormik } from "formik";
+import { passwordValidate } from "../helper/validate";
+// import { useAuthStore } from '../store/store'
 
 function Recovery() {
+  const formik = useFormik({
+    initialValues: {
+      otp: "",
+    },
+    validate: passwordValidate,
+    validateOnBlur: false,
+    validateOnChange: false,
+    onSubmit: async (values) => {
+      console.log(values);
+    },
+  });
+
   return (
-    <div>Recovery</div>
-  )
+    <div className="container mx-auto">
+      <Toaster position="top-center" reverseOrder={false}></Toaster>
+
+      <div className="flex justify-center items-center h-screen">
+        <div className={styles.glass}>
+          <div className="title flex flex-col items-center">
+            <h4 className="text-5xl font-bold">Recovery</h4>
+            <span className="py-4 text-xl w-2/3 text-center text-gray-500">
+              Enter OTP to recover your password.
+            </span>
+          </div>
+
+          <form className="pt-20" onSubmit={formik.handleSubmit}>
+            <div className="textbox flex flex-col items-center gap-3">
+              <div className="input text-center">
+                <span className="py-4 text-sm text-left text-gray-500">
+                  Enter 6 digit OTP sent to your email address.
+                </span>
+                <input
+                  className={styles.textbox}
+                  type="text"
+                  placeholder="OTP"
+                />
+              </div>
+              <button className={styles.btn} type="submit">
+                Recover Now
+              </button>
+            </div>
+
+            <div className="text-center">
+              <span className="text-gray-500">
+                Can&apos;t get OPT ?{" "}
+                <Link className="text-red-500" to="/recovery">
+                  Resend
+                </Link>
+              </span>
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Recovery;
