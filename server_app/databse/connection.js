5c37d8249399d1105add4018d835c53f0fee2d55
Subham Pradhan <59175226+SGsubuuu-2322@users.noreply.github.com>
2024-10-03 21:24:34 +0530

    Debug some scripts and databse connection issues...

--- server_app/databse/connection.js
+++ server_app/databse/connection.js
@@ -1,13 +1,13 @@
 import mongoose from "mongoose";
-import { MongoMemoryServer } from "mongodb-memory-server";
+// import { MongoMemoryServer } from "mongodb-memory-server";
 
 export default async function connect() {
-  const mongod = await MongoMemoryServer.create();
-  const getUri = mongod.getUri();
+  // const mongod = await MongoMemoryServer.create();
+  // const getUri = mongod.getUri();
 
-  mongoose.set("strictQuery", true);
+  // mongoose.set("strictQuery", true);
 
-  const db = await mongoose.connect(getUri);
+  const db = await mongoose.connect("mongodb://localhost:27017/MERN_LOGIN-APP");
   console.log("Databse Connected!!!");
   return db;
 }
