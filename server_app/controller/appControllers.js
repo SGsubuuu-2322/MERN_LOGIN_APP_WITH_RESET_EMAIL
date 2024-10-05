f1e3f761ba378dbb3e4a66d6b0dad9e82bb08652
Subham Pradhan <59175226+SGsubuuu-2322@users.noreply.github.com>
2024-10-06 00:57:09 +0530

    Created a middleware for authenticating a user with its username...

--- server_app/controller/appControllers.js
+++ server_app/controller/appControllers.js
@@ -3,6 +3,19 @@ import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
 import ENV from "../config.js";
 
+export async function verifyUser(req, res, next) {
+  try {
+    const { username } = req.method == "GET" ? req.query : req.body;
+    const user = await UserModel.findOne({ username });
+    if (!user) {
+      return res.status(404).json({ message: "User not found!!!" });
+    }
+    next();
+  } catch (error) {
+    return res.status(404).json({ error: "Authentication error..." });
+  }
+}
+
 /** POST: http://localhost:8080/api/register 
  * @param : {
   "username" : "example123",
